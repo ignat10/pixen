6d342edf7838d2622620b3c489b3a0167e4f849b
kazuru <ignat.shikh@gmail.com>
2026-06-24 17:26:07 +0200

    add one more test

--- tests/find_nth.rs
+++ tests/find_nth.rs
@@ -7,6 +7,9 @@ use images::{NTH_SCREEN, PNG_SAMPLE};
 fn found_nth() {
     let result = find_nth(&*NTH_SCREEN, &*PNG_SAMPLE, 0,6);
     assert_eq!(result, Some([394, 928]));
+
+    let result = find_nth(&*NTH_SCREEN, &*PNG_SAMPLE, 10,6);
+    assert_eq!(result, Some([394, 928]));
 }
 
 #[test]
